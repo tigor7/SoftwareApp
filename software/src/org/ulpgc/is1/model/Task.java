72423e45239a3d70eba06efc3bc60d41e047d3ff
Santiago Pujol <santiagopujol7@gmail.com>
2023-10-20 16:54:10 +0100

    Added Task methods

--- software/src/org/ulpgc/is1/model/Task.java
+++ software/src/org/ulpgc/is1/model/Task.java
@@ -8,4 +8,42 @@ public class Task {
     private Date start;
     private Date end;
 
+    public Task(String name, String description, Date start, Date end) {
+        this.name = name;
+        this.description = description;
+        this.start = start;
+        this.end = end;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getStart() {
+        return start;
+    }
+
+    public void setStart(Date start) {
+        this.start = start;
+    }
+
+    public Date getEnd() {
+        return end;
+    }
+
+    public void setEnd(Date end) {
+        this.end = end;
+    }
 }
