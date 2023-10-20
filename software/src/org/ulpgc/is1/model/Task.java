5e8e678aa36ea943c9f64229c0f8d939d551f1d3
Santiago Pujol <santiagopujol7@gmail.com>
2023-10-20 11:03:03 +0100

    Added packages and task class

---
+++ software/src/org/ulpgc/is1/model/Task.java
@@ -0,0 +1,10 @@
+package org.ulpgc.is1.model;
+
+import java.util.Date;
+
+public class Task {
+    private String name;
+    private String description;
+    private Date start;
+    private Date end;
+}
