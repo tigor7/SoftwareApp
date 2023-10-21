ab1b5b9dc61d8a6c774a96362e30ea25b337b2db
Santiago Pujol <santiagopujol7@gmail.com>
2023-10-21 20:19:34 +0100

    Added init

--- software/src/org/ulpgc/is1/control/Main.java
+++ software/src/org/ulpgc/is1/control/Main.java
@@ -1,8 +1,22 @@
 package org.ulpgc.is1.control;
 
-public class Main {
-    public static void main(String[] args) {
+import org.ulpgc.is1.model.Phone;
+import org.ulpgc.is1.model.ProjectManager;
 
+public class Main {
+    private static void init(ProjectManager manager) {
+        // i create two clients
+        manager.addCustomer("John", "Doe", new Phone("123456789"));
+        manager.addCustomer("Mary", "Jane", new Phone("987654321"));
+
+        // ii create two employees
+        manager.addEmployee("Jack", "jack@example.com");
+        manager.addEmployee("Joe", "joe@example.com");
+
+    }
+    public static void main(String[] args) {
+        ProjectManager manager = new ProjectManager();
+        init(manager);
         System.out.print("Hello and welcome! :DD");
         System.out.print("Hello");
 
