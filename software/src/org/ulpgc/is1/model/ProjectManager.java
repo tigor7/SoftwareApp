d207f5895f9501873485cba9a3e749279fadedae
Santiago Pujol <santiagopujol7@gmail.com>
2023-10-21 20:00:56 +0100

    Implemented project manager

--- software/src/org/ulpgc/is1/model/ProjectManager.java
+++ software/src/org/ulpgc/is1/model/ProjectManager.java
@@ -1,4 +1,30 @@
 package org.ulpgc.is1.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ProjectManager {
+    private List<Customer> customers;
+    private List<Employee> employees;
+
+    public ProjectManager() {
+        this.customers = new ArrayList<Customer>();
+        this.employees = new ArrayList<Employee>();
+
+    }
+
+    public void addCustomer(String name, String surname, Phone phone) {
+        customers.add(new Customer(name, surname, phone));
+    }
+
+    public void addEmployee(String name, String email) {
+        employees.add(new Employee(name, email));
+    }
+    public Customer getCustomer(int index) {
+        return customers.get(index);
+    }
+
+    public Employee getEmployee(int index) {
+        return employees.get(index);
+    }
 }
